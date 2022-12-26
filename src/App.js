bd58ef9b1c915d636f5cf09897d1d98d40904210
Darshana Perera <darshana.saluka.10@gmail.com>
2022-12-26 22:18:39 +0530

    footer created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
 import Design from "./Design/Design";
 
 function App() {
--- src/Design/Common/Footer.js
+++ src/Design/Common/Footer.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <p style={{ fontSize: "14px", textAlign: "center" }}>
+        All Rights Reserved. Platform is developed by &nbsp;
+        <a href="" style={{ textDecoration: "none" }} className="footer-link">
+          DSPerera.com
+        </a>
+      </p>
+    </div>
+  );
 }
