0cb4e2c8986c7baf0b55227017d7013981c881b6
Darshana Perera <darshana.p@hsenidmobile.com>
2023-01-14 18:46:34 +0530

    home page down part created

---
+++ src/Design/Components/Homepage/AboutUsSession.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function AboutUsSession() {
+  return (
+    <div style={{ padding: "2% 0px" }}>
+      <p className="justifyPara">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries,
+      </p>
+      <p className="justifyPara">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries,
+      </p>
+    </div>
+  );
+}
---
+++ src/Design/Components/Homepage/PoweredByLogoSet.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PoweredByLogoSet() {
+  return (
+    <div style={{dislplay:"flex",justifyContent:"space-around"}}>
+      <img src="" alt="Logo 01" />
+      <img src="" alt="Logo 01" />
+    </div>
+  );
+}
