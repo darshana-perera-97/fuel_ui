083e97b367351f61e87e08346dcb06ec79fdec3a
Darshana Perera <darshana.p@hsenidmobile.com>
2023-02-26 22:04:06 +0530

    qr code added

--- src/Design/Pages/ViewQRCode.js
+++ src/Design/Pages/ViewQRCode.js
@@ -2,16 +2,32 @@ import React from "react";
 import QRCode from "react-qr-code";
 
 export default function ViewQRCode() {
-  const value = 20;
+  const t = Math.round(Math.random() * 100000);
+  const value = t;
   return (
     <div>
       <div className="page">
+        <p className="title1 tcenter">QR Code for NW BCG 2072</p>
+        <p className="title6 tcenter">
+          Here's the QR code OR Pass Code{" "}
+          <span style={{ color: "#5733ad", opacity: "1.0" }}>
+            &lt;QR.code&gt;
+          </span>
+        </p>
+        <br />
+        <p className="title2 tcenter">Pass Code : {t}</p>
+        <br />
+        <br />
+        <p className="title1 tcenter">QR Code </p>
+        <br />
         <div
           style={{
             height: "auto",
             maxWidth: 100,
             width: "100%",
-            marginLeft:"40%"
+            marginLeft: "40%",
+            boxShadow: "5px 4px 7px #000000",
+            // border: "5px solid black",
           }}
         >
           <QRCode
@@ -21,6 +37,9 @@ export default function ViewQRCode() {
             viewBox={`0 0 20 26`}
           />
         </div>
+        <p className="title3 tcenter" style={{ marginTop: "5px" }}>
+          {t}
+        </p>
       </div>
     </div>
   );
